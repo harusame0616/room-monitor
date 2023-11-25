a8fbbd65189c1208e3dca92d96edd93f574073dd
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-11-25 16:35:33 +0900

    ２つ目以降のセンシングデータ追加に失敗する不具合修正

--- room-monitor/src/infrastructures/d1-sensing.repository.ts
+++ room-monitor/src/infrastructures/d1-sensing.repository.ts
@@ -5,8 +5,8 @@ export class D1SensingRepository implements SensingRepository {
 	constructor(private readonly db: D1Database) {}
 	async save(sensing: Sensing): Promise<void> {
 		await this.db
-			.prepare('INSERT INTO SENSING (DEVICE_ID, RELATIVE_HUMIDITY, ABSOLUTE_HUMIDITY, TEMPERATURE) VALUES (?, ?, ?, ?)')
-			.bind(sensing.id, sensing.relativeHumidity.value, sensing.absoluteHumidity.value, sensing.temperature.value)
+			.prepare('INSERT INTO SENSING (SENSING_ID, DEVICE_ID, RELATIVE_HUMIDITY, ABSOLUTE_HUMIDITY, TEMPERATURE) VALUES (?, ?, ?, ?, ?)')
+			.bind(sensing.sensingId, sensing.deviceId, sensing.relativeHumidity.value, sensing.absoluteHumidity.value, sensing.temperature.value)
 			.run();
 	}
 }
